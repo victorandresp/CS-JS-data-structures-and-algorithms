4003427c60fe52d3f219049b0d2359f4c0664a31
victorandresp <ponceottamendi27@gmail.com>
2023-03-30 18:43:57 -0400

    feat: linear search :sparkles:

---
+++ algorithms/search/linealSearch.js
@@ -0,0 +1,10 @@
+function findIndex(arr, value){
+    let index = -1
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i] === value)
+            return index = i
+    }
+    return index
+}
+const array = [-2, 1, 3, 19, 20, 9]
+console.log(findIndex(array, 99))

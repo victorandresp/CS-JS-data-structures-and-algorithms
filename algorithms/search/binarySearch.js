6dcf275fc75b1deb8ccb06f51671bdba7823e9cd
victorandresp <ponceottamendi27@gmail.com>
2023-03-30 22:19:49 -0400

    feat: binary search correct way :sparkles:

--- algorithms/search/binarySearch.js
+++ algorithms/search/binarySearch.js
@@ -1,4 +1,4 @@
-function binarySearch(arr, value){
+function binarySearch(arr, value){ // Maded my selft without kwoledgement
     if(!arr || arr.length === 0){
         return -1
     }
@@ -22,3 +22,28 @@ function binarySearch(arr, value){
 }
 const array = [-2, 1, 2, 4, 8, 10, 13] // for binary search, the array must mandatory be in order
 console.log(binarySearch([], 55));
+
+// Correct way
+function correctBinarySearch(arr, value){
+    let start = 0
+    let end = arr.length - 1
+
+    while(start <= end){
+        let middle = Math.floor((start + end) / 2)
+        if(arr[middle] === value){
+            return middle
+        }
+        
+        if(value > arr[middle]){
+            start = middle + 1
+        }else{
+            end = middle - 1
+        }
+    }
+
+    return -1
+}
+
+const arraytest = [-2, 1, 2, 4, 8, 10, 13] // for binary search, the array must mandatory be in order
+console.log('solve:', correctBinarySearch(arraytest, 8));
+
